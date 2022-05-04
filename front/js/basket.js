83621b72249f0b4ae0719864e283b1030573bae7
lq <liting_qiu@hotmail.com>
2022-05-04 20:04:14 +0200

    add getTotalQuantity,getTotalPrice method to class

--- front/js/basket.js
+++ front/js/basket.js
@@ -8,31 +8,37 @@ class ShoppingCart {
     }
 
     save (shoppingCart) {
-        localStorage.setItem("shoppingCart", JSON.stringify(shoppingCart));
+        this.cart=shoppingCart;
+        localStorage.setItem("shoppingCart", JSON.stringify(this.cart)); 
     }
+    // Question à voir avec mentor:
+    // pourquoi sans changer la valuer du proprité cart; je ne pouvais pas récuperer les bonnes valeurs après changement du quantité ?
+    // save(shoppingCart){
+    //     localStorage.setItem("shoppingCart", JSON.stringify(shoppingCart));  
+    // }
 
-    add (item) {
+    add (product) {
         if (this.cart.length === 0) {
-            this.cart.push(item);
+            this.cart.push(product);
             this.save(this.cart);
         }else {
-            const isSameId = this.cart.some( element => element.id === item.id );
-            const isSameColor = this.cart.some( element => element.id === item.id && element.color === item.color );
+            const isSameId = this.cart.some( element => element.id === product.id );
+            const isSameColor = this.cart.some( element => element.id === product.id && element.color === product.color );
             
             if (!isSameId ) {
-                this.cart.push(item);
+                this.cart.push(product);
                 this.save(this.cart);
             } 
     
             if (isSameId && !isSameColor) {
-                this.cart.push(item);
+                this.cart.push(product);
                 this.save(this.cart);
             }
     
             if (isSameId && isSameColor) {
                 const newShoppingCart = this.cart.map( element => {
-                    if (element.id === item.id && element.color === item.color) {
-                        return {...element,quantity: element.quantity + item.quantity}
+                    if (element.id === product.id && element.color === product.color) {
+                        return {...element,quantity: element.quantity + product.quantity}
                     }
                     return element;
                 })
@@ -52,7 +58,38 @@ class ShoppingCart {
             }
         })
         this.save(newShoppingCart); 
+       
     }
+
+    delete(id,color){
+        console.log(this.cart);
+        const newShoppingCart = this.cart.filter( element => {
+            return !(element.id === id && element.color === color)
+        })
+        this.save(newShoppingCart); 
+        
+    }
+
+    getTotalQuantity(){
+        let TotalQuantity = this.cart.reduce( (accumulateur,valeurCourante) => {
+            return accumulateur+valeurCourante.quantity
+        },0);
+
+        return TotalQuantity;
+    }
+
+    getTotalPrice(arrIncludePrice){
+        let sum = 0;
+        this.cart.forEach(element => {
+            const selectProduct = arrIncludePrice.find( (product)=> {
+                return product._id === element.id
+            });
+            sum += element.quantity * selectProduct.price;    
+        });
+        return sum;
+    }
+
+
 
 }
 
