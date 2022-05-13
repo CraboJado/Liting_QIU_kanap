1f3200820b3874f2c7b4befb05a54e6db11fc6c2
lq <liting_qiu@hotmail.com>
2022-05-13 16:08:58 +0200

    add thanks msg on confirmation page

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -2,4 +2,4 @@ const orderIdNode = document.querySelector('#orderId');
 const currentURL = window.location.href;
 const url = new URL(currentURL);
 const id = url.searchParams.get("id");
-orderIdNode.innerText = id ;
+orderIdNode.innerText = id + ', Merci pour votre visite, à bientôt ! ' ;
