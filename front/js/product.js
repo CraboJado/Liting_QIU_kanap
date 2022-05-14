82983abdb3833c1ad7c70529eb9d6809ee1d5f68
lq <liting_qiu@hotmail.com>
2022-05-15 01:07:03 +0200

    refactoring

--- front/js/product.js
+++ front/js/product.js
@@ -1,56 +1,94 @@
-const imgEle = document.querySelector(".item__img");
-const titleEle = document.querySelector("#title");
-const priceEle = document.querySelector("#price");
-const descripEle = document.querySelector("#description");
-const colorsEle = document.querySelector("#colors");
+const imgContainer = document.querySelector(".item__img");
 const addToCartBtn = document.querySelector("#addToCart");
-const quantityEle = document.querySelector("#quantity");
- 
+const quantityElement = document.querySelector("#quantity");
 // get product id from current URL
 const id = window.location.href.split("?")[1].split("=")[1];
 
-// add to cart 
-const addToCart = (id,colorsEle,quantityEle) => { 
-    const selectedColor = colorsEle.options[colorsEle.selectedIndex].value;
+const renderImg = data => {
+    const img = createElement('img');
+    appendElement(imgContainer,img);
+    const src = new Attribute('src',`${data.imageUrl}`);
+    const alt = new Attribute('alt',`Photographie d'un canapé ${data.name}`);
+    src.setAttribute(img);
+    alt.setAttribute(img);
+}
+
+const renderTitle = data => {
+    const titleElement = document.querySelector("#title");
+    setInnerText(titleElement,data.name);
+}
+
+const renderPrice = data => {
+    const priceElement = document.querySelector("#price");
+    setInnerText(priceElement ,data.price);
+}
+
+const renderDescription = data => {
+    const descripElement = document.querySelector("#description");
+    setInnerText(descripElement,data.description);
+}
+
+const renderColorOption = data => {
+    const colorsElement = document.querySelector("#colors");
+    for (i=0; i<data.colors.length; i++){
+        const colorOption = createElement('option');
+        const optionValue = new Attribute('value',`${data.colors[i]}`);
+        optionValue.setAttribute(colorOption);
+        insertElement(colorsElement,colorOption);
+        setInnerText(colorOption ,data.colors[i]);
+    }
+}
+
+const renderPage = data => {
+    // render img
+    renderImg(data);
+    // render title
+    renderTitle(data);
+    // render price
+    renderPrice(data);
+    // render discription
+    renderDescription(data);
+    // render colorOption dropdown menu
+    renderColorOption(data);
+}
+
+const getSelectedProduct = id => {
+    const colorsElement = document.querySelector("#colors");
+    const selectedColor = colorsElement.options[colorsElement.selectedIndex].value;
     const product = {
         id:id,
-        quantity: + quantityEle.value,
+        quantity: + quantityElement.value,
         color:selectedColor
     };
+    return product
+}
 
+const addToCart = () => {
+    const product = getSelectedProduct(id);
     if (product.color === "") {
         alert("veuillez sélectionner une couleur"); 
-    } else if (product.quantity === "0") {
+    } else if (product.quantity === 0 ) {
         alert("veuillez sélectionner une quantité");
     } else if (product.quantity > 100) {
         alert("la quantité maximum est 100");
-        quantityEle.value = 100;
+        quantityElement.value = 100;
     }else{
         const shoppingCart = new ShoppingCart();
         shoppingCart.add(product);
     }
 }
 
-// fetch data by id ,then render product page
-fetch(`http://localhost:3000/api/products/${id}`)
+const fetchDataById = (id) => {
+    return fetch(`http://localhost:3000/api/products/${id}`)
+};
+
+fetchDataById(id)
 .then( response  => response.json() )
 .then( data => {
-    imgEle.innerHTML =`<img src=${data.imageUrl} alt="Photographie d'un canapé ${data.name}">`; 
-    titleEle.innerText = data.name;
-    priceEle.innerText = data.price;
-    descripEle.innerText=data.description;
-
-    for (i=0; i<data.colors.length; i++){
-        colorsEle.insertAdjacentHTML("beforeend",`
-        <option value="${data.colors[i]}">${data.colors[i]}</option>`);
-    }
-
-    addToCartBtn.addEventListener('click', () => {
-        addToCart(id,colorsEle,quantityEle);
-    });
-
-}).catch( error => {
-    imgEle.innerHTML = error + ': le chargement de la page a rencontré un problème, veuillez re-essayer une prochain fois';
+    renderPage(data);
+    addToCartBtn.addEventListener('click', () => addToCart())
 })
+.catch( error => {imgContainer.innerText = error + ': le chargement de la page a rencontré un problème, veuillez re-essayer une prochain fois';})
+
 
 
