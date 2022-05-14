572e5839da6e332627bbf5f05238342412d69f18
lq <liting_qiu@hotmail.com>
2022-05-14 23:52:30 +0200

    using create element method to render page

--- front/js/script.js
+++ front/js/script.js
@@ -1,33 +1,75 @@
-// get product items container
-const itemsContainer = document.querySelector("#items");
+class Attribute {
+  constructor(name,value){
+    this.name = name;
+    this.value = value;
+  }
 
-// get products data
-fetch('http://localhost:3000/api/products')
+  setAttribute(element) {
+    element.setAttribute(this.name,this.value);
+  }
+}
+
+const createElement = element => {
+  return document.createElement(element);
+}
+
+const insertElement = (fartherNode, childNode) => {
+  fartherNode.insertAdjacentElement('beforeend',childNode);
+}
+
+const appendElement = (fartherNode, childNode) => {
+  fartherNode.appendChild(childNode);
+}
+
+const setInnerText =  (element,value) => {
+  element.innerText = value;
+}
+
+const renderPage = data => {
+  const itemsElement = document.querySelector('#items');
+
+  data.forEach( product => {
+    const aElement = createElement('a');
+    const articleElement = createElement('article');
+    const imgElement = createElement('img');
+    const titleElement = createElement('h3');
+    const descElement = createElement('p');
+    insertElement(itemsElement,aElement);
+    appendElement(aElement,articleElement);
+    appendElement(articleElement,imgElement);
+    insertElement(articleElement,titleElement);
+    insertElement(articleElement,descElement);
+
+    const href = new Attribute('href',`./product.html?id=${product._id}`);
+    const src = new Attribute('src',`${product.imageUrl}`);
+    const alt = new Attribute('alt',`Lorem ipsum dolor sit amet,${product.name}`);
+    const titleClassName = new Attribute('class','productName');
+    const descriptionClassName = new Attribute('class','productDescription');
+    href.setAttribute(aElement);
+    src.setAttribute(imgElement);
+    alt.setAttribute(imgElement);
+    titleClassName.setAttribute(titleElement);
+    descriptionClassName.setAttribute(descElement);
+
+    setInnerText(titleElement,product.name);
+    setInnerText(descElement,product.description);
+  })
+
+}
+
+const fetchProdutData = () => {
+  return fetch('http://localhost:3000/api/products')
+}
+
+fetchProdutData()
 .then( response => response.json() )
 .then( data => {
-
-  let listItems ="";
-
-    data.forEach( item => {
-      listItems +=`
-      <a href="./product.html?id=${item._id}">
-        <article>
-          <img src="${item.imageUrl}" alt="Lorem ipsum dolor sit amet, Kanap name1">
-          <h3 class="productName">${item.name}</h3>
-          <p class="productDescription">${item.description}</p>
-        </article>
-      </a>`;
-
-    } )
-
-    itemsContainer.innerHTML = listItems;
-    
-    // save products data in LocalStorage
-    localStorage.setItem("productData",JSON.stringify(data));
-
-}).catch( error => {
-  itemsContainer.innerHTML = error + ': le chargement de la page a rencontré un problème, veuillez re-essayer une prochain fois';
-});
+    // render page
+    renderPage(data)
+    // save product data in LocalStorage
+    localStorage.setItem('productData',JSON.stringify(data));
+})
+.catch( error => {itemsElement.innerHTML = error + ': le chargement de la page a rencontré un problème, veuillez re-essayer une prochain fois';});
 
 
 
