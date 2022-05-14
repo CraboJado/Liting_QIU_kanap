28d66b1b845e0d12296e8cd6d4ca54aa2d6847b1
lq <liting_qiu@hotmail.com>
2022-05-15 00:58:22 +0200

    remove utility functions and class

--- front/js/script.js
+++ front/js/script.js
@@ -1,29 +1,3 @@
-class Attribute {
-  constructor(name,value){
-    this.name = name;
-    this.value = value;
-  }
-
-  setAttribute(element) {
-    element.setAttribute(this.name,this.value);
-  }
-}
-
-const createElement = element => {
-  return document.createElement(element);
-}
-
-const insertElement = (fartherNode, childNode) => {
-  fartherNode.insertAdjacentElement('beforeend',childNode);
-}
-
-const appendElement = (fartherNode, childNode) => {
-  fartherNode.appendChild(childNode);
-}
-
-const setInnerText =  (element,value) => {
-  element.innerText = value;
-}
 
 const renderPage = data => {
   const itemsElement = document.querySelector('#items');
